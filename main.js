31ae0428c57b293be06dcebea853205a97c3482d
Rus1130 <arirogozi@gmail.com>
2026-03-09 21:52:27 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ const os = new OS(document.getElementById('terminal'));
 
 os.line('Welcome to swagOS2 ===', "===");
 os.commandLine();
-os.sendCommand('p c/u | lc')
+// os.sendCommand('p c/u | lc')
 // os.sendCommand("p c/u | service logs | service logs")
 // os.sendCommand("service logs | p c/u | service logs")
